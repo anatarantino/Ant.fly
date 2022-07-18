86a76ac74c9f411b8106bbecb6555ec1d4dba75d
Anita <atarantino@itba.edu.ar>
2022-07-18 13:33:05 -0300

    Register and login validation

--- bitly/src/app/main/login/login.component.ts
+++ bitly/src/app/main/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/services/auth.service';
 import {ApiService} from '../../services/api.service'
 
 @Component({
@@ -12,33 +12,52 @@ import {ApiService} from '../../services/api.service'
 export class LoginComponent implements OnInit {
   form: FormGroup
   disabled = false
+  minEmailLength: number = 6;
+  maxEmailLength: number = 254;
+  minPassLength: number = 6;
+  maxPassLength: number = 30;
+
   constructor(
-    private _api : ApiService,
+    private _api: ApiService,
     private _auth: AuthService,
     private router: Router,
     public fb: FormBuilder
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      email: ['', Validators.required],
-      password:['', Validators.required],
-      username:['']
-
-    });
-
+      email: ['',
+        Validators.required,
+        Validators.maxLength(this.maxEmailLength),
+        Validators.minLength(this.minEmailLength),
+        Validators.pattern('^([a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+)*$')],
+      password: ['',
+        Validators.required,
+        Validators.minLength(this.minPassLength),
+        Validators.maxLength(this.maxPassLength)],
+      username: ['',]
+    })
+    ;
   }
 
-  login(){
+  login() {
+    this.disabled=true;
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      this.disabled = false;
+      return;
+    }
     let b = this.form.value
     this._api.postTypeRequest('login', b).subscribe((res: any) => {
       console.log(res)
-      if(res.access_token){
-				res.access_token = res.access_token.replace(/^"(.*)"$/, '$1'); // Remove quotes from token start/end. This is a temp fix?!
+      if (res.access_token) {
+        res.access_token = res.access_token.replace(/^"(.*)"$/, '$1'); // Remove quotes from token start/end. This is a temp fix?!
         this._auth.setDataInLocalStorage('token', res.access_token)
         this.router.navigate(['home'])
       }
     }, err => {
+      this.disabled = false;
       console.log(err)
     });
   }
--- bitly/src/app/main/register/register.component.ts
+++ bitly/src/app/main/register/register.component.ts
@@ -20,18 +20,38 @@ export class RegisterComponent implements OnInit {
 
   form: FormGroup
   disabled = false
+  minEmailLength: number = 6;
+  maxEmailLength: number = 254;
+  minPassLength: number = 6;
+  maxPassLength: number = 30;
+  minUserLength: number = 3;
+  maxUserLength: number = 30;
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      username: ['', Validators.required]
-    });
+      email: ['',
+        Validators.required,
+        Validators.maxLength(this.maxEmailLength),
+        Validators.minLength(this.minEmailLength),
+        Validators.pattern('^([a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+)*$')],
+      password: ['',
+        Validators.required,
+        Validators.minLength(this.minPassLength),
+        Validators.maxLength(this.maxPassLength)],
+      username: ['',
+        Validators.required,
+        Validators.minLength(this.minUserLength),
+        Validators.maxLength(this.maxUserLength)]
+    })
+    ;
   }
 
   register() {
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
     let b = this.form.value
-
     this._api.postTypeRequest('register', b).subscribe((res: any) => {
       if (res.status_code == 200) {
         this._api.postTypeRequest('login', b).subscribe((res: any) => {
